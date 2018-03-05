2d1101e219550eeca47734b35f2750d9d2dad6d9
Daniel Gaunled <gaunled@gmail.com>
2018-03-05 22:47:33 +0700

    create model in mysql

---
+++ src/restoranmujigae/main/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restoranmujigae.main;
+
+public class Main {
+
+    public Main() {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
