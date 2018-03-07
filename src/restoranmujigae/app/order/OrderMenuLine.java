f96922edba0b543eba8d364731799d4c6c3b6d79
Daniel Gaunled <gaunled@gmail.com>
2018-03-07 15:39:01 +0700

    model update

--- src/restoranmujigae/app/order/OrderMenuLine.java
+++ src/restoranmujigae/app/order/OrderMenuLine.java
@@ -17,5 +17,70 @@ public class OrderMenuLine {
     private boolean is_deleted;
     private OrderMenu order;
     private Menu menu;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public int getId_order() {
+        return id_order;
+    }
+
+    public void setId_order(int id_order) {
+        this.id_order = id_order;
+    }
+
+    public int getId_menu() {
+        return id_menu;
+    }
+
+    public void setId_menu(int id_menu) {
+        this.id_menu = id_menu;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public boolean isIs_deleted() {
+        return is_deleted;
+    }
+
+    public void setIs_deleted(boolean is_deleted) {
+        this.is_deleted = is_deleted;
+    }
+
+    public OrderMenu getOrder() {
+        return order;
+    }
+
+    public void setOrder(OrderMenu order) {
+        this.order = order;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+    
     
 }
