37bbf562fae64fc6c67af9ee1cc1f80bbdc5882d
Glenn Hizkia Naftali <hizkianaft@gmail.com>
2018-03-09 15:48:41 +0700

    UI/UX Finished
    nnti tinggal janjian id attribute aja barengan

--- src/restoranmujigae/main/Main.java
+++ src/restoranmujigae/main/Main.java
@@ -5,13 +5,27 @@
  */
 package restoranmujigae.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public Main() {
-        
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/Dessert.fxml"));
+        primaryStage.setTitle("MUJIGAE");
+        primaryStage.setMaximized(false);
+        primaryStage.setFullScreen(true);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
-    
+
     public static void main(String[] args) {
-        
+        launch(args);
     }
+
+    
 }
