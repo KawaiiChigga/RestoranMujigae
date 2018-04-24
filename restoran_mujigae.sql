116df6ef9d3ee3cbc95b2eb79687d3a267bb38d6
Glenn Hizkia Naftali <hizkianaft@gmail.com>
2018-04-25 03:01:57 +0700

    Checkout Cashier, (Waiter + Cashier masih error jadi ga bsa di klik)

---
+++ restoran_mujigae.sql
@@ -0,0 +1,266 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2018 at 07:57 PM
+-- Server version: 10.1.24-MariaDB
+-- PHP Version: 7.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `restoran_mujigae`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cashier`
+--
+
+CREATE TABLE `cashier` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `status` tinyint(1) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `deskripsi` varchar(256) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `img_url` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meja`
+--
+
+CREATE TABLE `meja` (
+  `id` int(11) NOT NULL,
+  `noMeja` varchar(25) NOT NULL,
+  `status` tinyint(1) DEFAULT '1',
+  `callwaiter` tinyint(1) NOT NULL DEFAULT '0',
+  `callcheckout` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `harga` decimal(10,0) NOT NULL,
+  `img_url` varchar(256) DEFAULT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `status` tinyint(1) DEFAULT '1',
+  `deskripsi` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_menu`
+--
+
+CREATE TABLE `order_menu` (
+  `id` int(11) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `id_meja` int(11) NOT NULL,
+  `id_waiter` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_deleted` tinyint(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_menu_line`
+--
+
+CREATE TABLE `order_menu_line` (
+  `id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `harga` decimal(10,0) NOT NULL,
+  `id_order` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_deleted` tinyint(1) DEFAULT '0',
+  `status` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `id` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_order` int(11) NOT NULL,
+  `id_cashier` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_deleted` tinyint(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `waiter`
+--
+
+CREATE TABLE `waiter` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `pin` varchar(256) NOT NULL,
+  `status` tinyint(1) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cashier`
+--
+ALTER TABLE `cashier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `meja`
+--
+ALTER TABLE `meja`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_menu`
+--
+ALTER TABLE `order_menu`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_order_meja` (`id_meja`),
+  ADD KEY `fk_order_waiter` (`id_waiter`);
+
+--
+-- Indexes for table `order_menu_line`
+--
+ALTER TABLE `order_menu_line`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_orderline_menu` (`id_menu`),
+  ADD KEY `fk_orderline_order` (`id_order`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_transaction_order` (`id_order`),
+  ADD KEY `fk_transaction_cashier` (`id_cashier`);
+
+--
+-- Indexes for table `waiter`
+--
+ALTER TABLE `waiter`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cashier`
+--
+ALTER TABLE `cashier`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `meja`
+--
+ALTER TABLE `meja`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `order_menu`
+--
+ALTER TABLE `order_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `order_menu_line`
+--
+ALTER TABLE `order_menu_line`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `waiter`
+--
+ALTER TABLE `waiter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order_menu`
+--
+ALTER TABLE `order_menu`
+  ADD CONSTRAINT `fk_order_meja` FOREIGN KEY (`id_meja`) REFERENCES `meja` (`id`),
+  ADD CONSTRAINT `fk_order_waiter` FOREIGN KEY (`id_waiter`) REFERENCES `waiter` (`id`);
+
+--
+-- Constraints for table `order_menu_line`
+--
+ALTER TABLE `order_menu_line`
+  ADD CONSTRAINT `fk_orderline_menu` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id`),
+  ADD CONSTRAINT `fk_orderline_order` FOREIGN KEY (`id_order`) REFERENCES `order_menu` (`id`);
+
+--
+-- Constraints for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `fk_transaction_cashier` FOREIGN KEY (`id_cashier`) REFERENCES `cashier` (`id`),
+  ADD CONSTRAINT `fk_transaction_order` FOREIGN KEY (`id_order`) REFERENCES `order_menu` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/restoranmujigae/view/controller/CashierMenuCtrl.java
+++ src/restoranmujigae/view/controller/CashierMenuCtrl.java
@@ -19,6 +19,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.Label;
@@ -28,10 +29,12 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import restoranmujigae.app.controller.data.DataCtrl;
+import restoranmujigae.app.controller.order.OrderCtrl;
 import restoranmujigae.app.model.order.Meja;
+import restoranmujigae.main.Main;
 
 public class CashierMenuCtrl {
-    
+
     @FXML
     private Button meja1;
     @FXML
@@ -56,7 +59,200 @@ public class CashierMenuCtrl {
     private Button meja11;
     @FXML
     private Button meja12;
+
+    @FXML
+    private void getMeja1(ActionEvent event) throws IOException {
+        System.out.println("MEJAAAAAAAAAAAAAAAAAAAAAAAAAAA 1");
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/CashierCheckout.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+    }
+
+    @FXML
+    private void getMeja2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/CashierCheckout.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+        stage.setFullScreen(true);
+    }
+
+    @FXML
+    private void getMeja3(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/CashierCheckout.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+        stage.setFullScreen(true);
+    }
+
+    @FXML
+    private void getMeja4(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/CashierCheckout.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+        stage.setFullScreen(true);
+    }
+
+    @FXML
+    private void getMeja5(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/CashierCheckout.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+        stage.setFullScreen(true);
+    }
+
+    @FXML
+    private void getMeja6(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/CashierCheckout.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+        stage.setFullScreen(true);
+    }
+
+    @FXML
+    private void getMeja7(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/CashierCheckout.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+        stage.setFullScreen(true);
+    }
+
+    @FXML
+    private void getMeja8(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/CashierCheckout.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+        stage.setFullScreen(true);
+    }
+
+    @FXML
+    private void getMeja9(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/CashierCheckout.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+        stage.setFullScreen(true);
+    }
+
+    @FXML
+    private void getMeja10(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/CashierCheckout.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+        stage.setFullScreen(true);
+    }
+
+    @FXML
+    private void getMeja11(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/CashierCheckout.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+        stage.setFullScreen(true);
+    }
+
+    @FXML
+    private void getMeja12(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/CashierCheckout.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+        stage.setFullScreen(true);
+    }
+    @FXML
+    private void getCashier(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/restoranmujigae/view/Cashier.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("MUJIGAE");
+        stage.setMaximized(true);
+        stage.setResizable(true);
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());
+        stage.show();
+        stage.setFullScreen(true);
+    }
     
+      @FXML
+    private void checkoutMeja(ActionEvent event) throws IOException {
+//       OrderCtrl.callCheckout(Main.ID_MEJA, 1);
+        
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText("Thank you!");
+        alert.setContentText("Please wait! Your order are being prepared");
+
+        alert.showAndWait();
+    }
+
     @FXML
     private void initialize() throws Exception {
         refresh();
@@ -70,29 +266,53 @@ public class CashierMenuCtrl {
         fiveSecondsWonder.setCycleCount(Timeline.INDEFINITE);
         fiveSecondsWonder.play();
     }
-    
+
     @FXML
     protected void getRefresh(ActionEvent event) throws IOException {
         refresh();
     }
-    
+
     public void refresh() {
         List<Meja> temp = DataCtrl.getAllAvailableMeja();
         for (int i = 0; i < temp.size(); i++) {
             boolean call = temp.get(i).isCallcheckout();
             switch (i) {
-                case 0: setColor(meja1, call); break;
-                case 1: setColor(meja2, call); break;
-                case 2: setColor(meja3, call); break;
-                case 3: setColor(meja4, call); break;
-                case 4: setColor(meja5, call); break;
-                case 5: setColor(meja6, call); break;
-                case 6: setColor(meja7, call); break;
-                case 7: setColor(meja8, call); break;
-                case 8: setColor(meja9, call); break;
-                case 9: setColor(meja10, call); break;
-                case 10: setColor(meja11, call); break;
-                case 11: setColor(meja12, call); break;
+                case 0:
+                    setColor(meja1, call);
+                    break;
+                case 1:
+                    setColor(meja2, call);
+                    break;
+                case 2:
+                    setColor(meja3, call);
+                    break;
+                case 3:
+                    setColor(meja4, call);
+                    break;
+                case 4:
+                    setColor(meja5, call);
+                    break;
+                case 5:
+                    setColor(meja6, call);
+                    break;
+                case 6:
+                    setColor(meja7, call);
+                    break;
+                case 7:
+                    setColor(meja8, call);
+                    break;
+                case 8:
+                    setColor(meja9, call);
+                    break;
+                case 9:
+                    setColor(meja10, call);
+                    break;
+                case 10:
+                    setColor(meja11, call);
+                    break;
+                case 11:
+                    setColor(meja12, call);
+                    break;
             }
         }
     }
@@ -104,5 +324,5 @@ public class CashierMenuCtrl {
             target.setStyle("-fx-background-color:#90caf9");
         }
     }
-    
+
 }
